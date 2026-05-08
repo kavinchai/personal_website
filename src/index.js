4f052a4600c27575f614f4c06610936406fdeb9c
Kavin <kavinchai00@gmail.com>
2026-05-07 23:09:17 -0400

    Added vercel analytics

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import App from "./App";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <App />
+  <>
+    <App />
+    <Analytics />
+  </>
 );
